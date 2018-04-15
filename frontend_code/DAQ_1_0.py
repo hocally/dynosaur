244082af8526b781e88eace4692084a61b0c0b2b
hocally <henry.ocallaghan@gmail.com>
2018-04-14 22:55:13 -0500

    Worked on the python code a lil

--- frontend_code/DAQ_1_0.py
+++ frontend_code/DAQ_1_0.py
@@ -2,9 +2,25 @@ import serial
 
 import time
 
-class Node
 
-	
+class Node:
+    def __init__(self, velocity, acceleration, previous):
+        self.velocity = velocity
+        self.acceleration = acceleration
+        self.previous = previous
+        self.next = None
 
-while(true)
+    def setNext(self, node):
+        self.next = node
 
+
+head = Node(None, None, None)
+tail = Node(None, None, head)
+
+ser = serial.Serial('/dev/tty', 9600) # Establish the connection on a specific port
+
+for x in range():
+    tail = Node(x, x, tail)
+    tail.previous.setNext(tail)
+
+print(tail.velocity)
