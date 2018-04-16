21a6e460734a399016e0602ae05a33b817ccf1fd
hocally <henry.ocallaghan@gmail.com>
2018-04-16 09:36:06 -0500

    Added basic plot framwork, need to figure out constant updating, and multiple datasets on same plot

--- frontend_code/DAQ_1_0.py
+++ frontend_code/DAQ_1_0.py
@@ -1,8 +1,8 @@
 import serial
-
+import csv
 import easygui
-
 import datetime
+import matplotlib.pyplot as plt
 
 
 class Queue:
@@ -21,11 +21,15 @@ class Queue:
 	def size(self):
 		return len(self.items)
 
+X = [590,540,740,130,810,300,320,230,470,620,770,250]
+Y = [32,36,39,52,61,72,77,75,68,57,48,48]
+
+plt.scatter(X, Y)
 
 #Program vars
 data = Queue()
 port = '/dev/ttyUSB0'
-ser = serial.Serial(port, 115200)
+#ser = serial.Serial(port, 115200)
 running = False
 run = 1
 
@@ -37,10 +41,13 @@ while True:
 	fieldValues = easygui.multenterbox("Please enter the following information about the test", "Dynosaur 1.0", testInfo)
 	flywheel = easygui.choicebox("Please select the flywheel being used for the test. \nUnits are in kg * m²", "Dynosaur 1.0", flywheels)
 	now = datetime.datetime.now()
-	fieldValues[0] = fieldValues[0].replace(" ", "")
-	fieldValues[1] = fieldValues[1].replace(" ", "")
-	filename = fieldValues[0] + "_" + fieldValues[1] + "_" + str(now.month) + "_" + str(now.day) + "_" + str(now.year)
+	filename = fieldValues[0].replace(" ", "") + "_" + fieldValues[1].replace(" ", "") + "_" + str(now.month) + "_" + str(now.day) + "_" + str(now.year)
 	path = easygui.diropenbox("Please select the directory to save the test data.", "Dynosaur 1.0", "~")
 	print(filename)
 	print(path)
 	break
+
+plt.title(fieldValues[0] + " Torque Curve")
+plt.xlabel("Angular Velocity (RPM)")
+plt.ylabel("Torque N*M")
+plt.show()
