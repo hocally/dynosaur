08955628e0531f50e7ee991d687514dde95c9d92
hocally <henry.ocallaghan@gmail.com>
2018-04-18 13:14:46 -0500

    Added som todo stuff

--- frontend_code/DAQ_1_0.py
+++ frontend_code/DAQ_1_0.py
@@ -3,17 +3,18 @@ import csv
 import easygui
 import datetime
 import matplotlib.pyplot as plt
+import time
 
-X = [590, 540, 740, 130, 810, 300, 320, 230, 470, 620, 770, 250]
-Y = [32, 36, 39, 52, 61, 72, 77, 75, 68, 57, 48, 48]
-
+velocity = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+acceleration = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 #Program vars
+title = "Dynosaur 1.0"
 port = '/dev/ttyUSB0'
 #ser = serial.Serial(port, 115200)
 logo = "logo.gif"
 testState = 0
-testRun = 1
+testRun = 0
 
 #Test vars
 flywheels = [69, 420, 1337]
@@ -25,30 +26,47 @@ runSwitch = {runChoices[0] : 0, runChoices[1] : 2, runChoices[2] : 1, runChoices
 
 while True:
 	if testState == 0:
-		reply = easygui.buttonbox("Please select the desired option.", choices = testChoices)
+		reply = easygui.buttonbox("Please select the desired option.", title, choices = testChoices)
 		if reply == logo:
 			testState = 0
 			continue
 		testState = testSwitch[reply]
 
 	elif testState == 1:
-		fieldValues = easygui.multenterbox("Please enter the following information about the test", "Dynosaur 1.0", testInfo)
+		fieldValues = easygui.multenterbox("Please enter the following information about the test", title, testInfo)
 		flywheel = easygui.choicebox("Please select the flywheel being used for the test. \nUnits are in kg * m²", "Dynosaur 1.0", flywheels)
 		now = datetime.datetime.now()
 		filename = fieldValues[0].replace(" ", "") + "_" + fieldValues[1].replace(" ", "") + "_" + str(now.month) + "_" + str(now.day) + "_" + str(now.year) + "_" + str(testRun)
-		path = easygui.diropenbox("Please select the directory to save the test data.", "Dynosaur 1.0", "~")
+		path = easygui.diropenbox("Please select the directory to save the test data.", title, "~")
 		testState = 2
 
 	elif testState == 2:
-		#run test
-		#view/save results
+		testRun += 1
+		#Wait for start command
+		go = easygui.boolbox("Begin test?", "Dynosaur 1.0")
+		#if go:
+		#	while True:
+				#Log data as it comes in
+
+				#Periodically update display
+
+				#Terminate test at RPM or time or button?
+
+				#View/save results
+
 		reply = easygui.buttonbox("What now?", choices=runChoices)
-		testState = testSwitch[reply]
+		testState = runSwitch[reply]
 
 	elif testState == 3:
-		#calibration?
-		print("Calibrating dyno")
+		#Calibration?
+
+		#Not totally sure what this should do
+
+		#Probably just run a "test" without requiring bike parameters?
 		testState = -1
 
 	else:
+		print("Exiting")
 		break
+
+def runTest():
